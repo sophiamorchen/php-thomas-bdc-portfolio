95395f315014a3a97f229e769175013f08c8aa08
Sophia M <sophia.moerchen@gmail.com>
2026-02-19 16:55:16 +0100

    Add font awesome and skills and connect to db

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio</title>
     <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-..."
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer" />
     <script src="assets/js/burger.js"></script>
 </head>
 
@@ -36,7 +40,7 @@
                     <article class="project-card">
 
                         <h3 class="project-card__title">
-                            <?php echo htmlspecialchars($project['title'], ENT_QUOTES, 'utf-8') . "\t";?>
+                            <?php echo htmlspecialchars($project['title'], ENT_QUOTES, 'utf-8') . "\t"; ?>
                         </h3>
 
                         <p class="project-card__description">
@@ -45,14 +49,17 @@
 
                         <div class="project-card__links">
                             <a
-                                href="<?php echo htmlspecialchars($project['github_link'], ENT_QUOTES, 'utf-8') . "\t";?>"
-                                class="project-card__link project-card__link--primary">Github
-                                
+                                href="<?php echo htmlspecialchars($project['github_link'], ENT_QUOTES, 'utf-8') . "\t"; ?>"
+                                class="project-card__link project-card__link--primary">
+                                <i class="fa-brands fa-github"></i>
+                                Github
+
                             </a>
 
                             <a
                                 href="https://app.studi.fr/v3/events/98006/stream?type=0"
                                 class="project-card__link project-card__link--outline">
+                                <i class="fa-solid fa-up-right-from-square"></i>
                                 Voir
                             </a>
                         </div>
@@ -60,18 +67,37 @@
                     </article>
                 <?php endforeach ?>
 
-
-                
-                
-
-
-                
             </div>
         </section>
 
 
         <section class="skills">
+            <h2 class="skills__title">Mes skills</h2>
 
+            <div class="skills__list">
+                <?php
+                $pdo = getDbConnection();
+                $sql = 'SELECT * FROM skills;';
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute();
+                $result = $stmt->fetchAll();
+
+                foreach ($result as $row): ?>
+                    <article class="skill-card">
+                        <?php if ($row['logo'] == null): ?>
+                            <h3 class="skill-card__title">
+                                <?php echo htmlspecialchars($row['name'], ENT_QUOTES, 'utf-8'); ?>
+                            </h3>
+                        <?php else : ?>
+                            <img 
+                            src="<?php echo htmlspecialchars($row['logo'], ENT_QUOTES, 'utf-8')?>"
+                            alt="Logo <?php echo htmlspecialchars($row['name'], ENT_QUOTES, 'utf-8'); ?>"
+                            >
+                        <?php endif ?>
+                    </article>
+                <?php endforeach ?>
+
+            </div>
         </section>
         <section class="references">
 
